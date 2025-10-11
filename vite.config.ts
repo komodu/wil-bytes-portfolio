f5b379e2e96634ac8fdb92bb036c6953449f7902
Wil Lorenz Dagli <willdagli@gmail.com>
2025-10-11 09:13:37 +0800

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-    base: '/wil-bytes-portfolio/',
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+export default defineConfig({
+  base: "/wil-bytes-portfolio", // relative paths work best with HashRouter
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+  build: {
+    outDir: "dist",
+  },
+});
